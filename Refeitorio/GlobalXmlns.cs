b8c0f279e8b5762d53c2e562fcaf3dda90588ab9
matda <matda@KAIO-MATIAS-TI>
2025-09-15 10:37:56 -0300

    okay

--- Refeitorio/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Refeitorio")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Refeitorio.Pages")]
